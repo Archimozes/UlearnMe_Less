8054ed713acd524b0b79ac0f6c92a90224e3c0be
ArchiMozes <Jokerus@inbox.ru>
2022-03-09 23:20:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// 1 Option
+var sum = Enumerable.Range(0, 1000).Where(i => i % 3 == 0 | i % 5 == 0).Sum();
+Console.WriteLine(sum);
+
+// 2 option
+sum = 0;
+for (int i = 0; i < 1000; i++)
+{
+    if (i % 3 == 0 | i % 5 == 0)
+    {
+        sum += i;
+    }
+}
+
+Console.WriteLine(sum);
